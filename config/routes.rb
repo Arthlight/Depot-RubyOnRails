2df3c8eb9723c989465931011a4bbde7ef56b455
arthlight <arthurpbh@gmail.com>
2020-12-21 15:06:32 +0100

    Added order form and completed corresponding action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'store#index', as: 'store_index'
   resources :line_items
   resources :carts
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
 
 
